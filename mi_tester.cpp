ccd8ec2eff67c6186727392be9bf8ec93fa64687
Brandon Mousseau <brandon.a.mousseau@gmail.com>
2017-02-16 19:14:38 -0800

    Updated some test code

--- mi_tester.cpp
+++ mi_tester.cpp
@@ -34,18 +34,18 @@ int main (void) {
     r->commandCode = DATA_RETURN_MEMORY;
     r->targetAddress = BROADCAST_ADX;
     r->sourceAddress = 2;
-    r->payload = 12345;
+    r->payload = 12320;
 
     BusRequest* k = new BusRequest();
     k->commandCode = DATA_RETURN_MEMORY;
     k->targetAddress = BROADCAST_ADX;
     k->sourceAddress = 2;
-    k->payload = 12346;
+    k->payload = 12800;
 
     BusRequest* gotit = c->initiateBusTransaction();
     c->acceptBusTransaction(k);
     c->Tick();
-
+    cout << "Sending expected data" << endl;
     c->acceptBusTransaction(r);
 
     c->Tick();
@@ -55,7 +55,7 @@ int main (void) {
     p->commandCode = BUSREAD;
     p->targetAddress = BROADCAST_ADX;
     p->sourceAddress = 2;
-    p->payload = 12345;
+    p->payload = 12320;
 
     c->Tick();
     c->acceptBusTransaction(p);
@@ -69,9 +69,10 @@ int main (void) {
     Instruction* i1 = new Instruction;
     
     i1->OPCODE = STORE_CMD;
-    i1->ADDRESS = 59684;
-    
+    i1->ADDRESS = 59392;
+    cout << "Before" << endl;
     c->handleMemoryAccess(i1);
+    cout << "After" << endl;
     
     c->Tick();
     c->Tick();
@@ -82,7 +83,7 @@ int main (void) {
     q->commandCode = BUSREAD;
     q->targetAddress = BROADCAST_ADX;
     q->sourceAddress = 2;
-    q->payload = 59684;
+    q->payload = 59392;
     
     c->acceptBusTransaction(q);
     c->Tick();
@@ -91,7 +92,7 @@ int main (void) {
     j->commandCode = DATA_RETURN_MEMORY;
     j->targetAddress = BROADCAST_ADX;
     j->sourceAddress = 2;
-    j->payload = 59684;
+    j->payload = 59392;
     
     c->acceptBusTransaction(j);
     c->Tick();
