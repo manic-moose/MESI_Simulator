9be2445cfbcdf0195b533737249af9ad0bc50c13
Brandon Mousseau <brandon.a.mousseau@gmail.com>
2017-02-20 13:17:04 -0800

    decremented opcode values

--- Instruction.h
+++ Instruction.h
@@ -1,9 +1,9 @@
 #ifndef INSTRUCTION_H
 #define INSTRUCTION_H
 
-#define LOAD_CMD  1
-#define STORE_CMD 2
-#define NOP_CMD   3
+#define LOAD_CMD  0
+#define STORE_CMD 1
+#define NOP_CMD   2
 
 struct Instruction {
     unsigned int OPCODE;
