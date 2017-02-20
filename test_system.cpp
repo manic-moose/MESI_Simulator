6bc2ad1289bd09e64f2fcf0c3f0c4c7f909e1ba8
Brandon Mousseau <brandon.a.mousseau@gmail.com>
2017-02-20 15:25:15 -0800

    incremented test code some

--- test_system.cpp
+++ test_system.cpp
@@ -2,25 +2,29 @@
 #include "Instruction.h"
 #include <stdlib.h>
 
-#define NUM_PROCESSORS 1
-
-#define NUM_TICKS 1000
+#define NUM_PROCESSORS 2
+#define MEMORY_LATENCY 10
+#define NUM_TICKS 100000
 
 int main(void) {
     
     srand(time(NULL));
     
-    System* s = new System(NUM_PROCESSORS, MESI_CONTROLLER_TYPE, 4);
+    System* s = new System(NUM_PROCESSORS, MESI_CONTROLLER_TYPE, MEMORY_LATENCY);
     
     for (int i = 0; i < NUM_TICKS; i++) {
-        s->Tick();
         for (int pNum = 0; pNum < NUM_PROCESSORS; pNum++) {
-            Instruction* inst = new Instruction;
-            inst->OPCODE  = rand() % 3;
-            inst->ADDRESS = rand() % 0xFFFFFFFF;
-            cout << "P" << pNum << " OPCODE: " << inst->OPCODE << "  Address: " << inst->ADDRESS << endl;
-            s->insertInstruction(inst,pNum);
+            if (!(s->hasPendingInstructions(pNum))) {
+                Instruction* inst = new Instruction;
+                inst->OPCODE  = rand() % 3;
+                inst->ADDRESS = rand() % 0xFFFFFFFF;
+                if (inst->OPCODE == LOAD_CMD || inst->OPCODE == STORE_CMD) {
+                    cout << "P" << pNum << " OPCODE: " << inst->OPCODE << "  Address: " << inst->ADDRESS << endl;
+                }
+                s->insertInstruction(inst,pNum);
+            }
         }
+        s->Tick();
     }        
     
     
